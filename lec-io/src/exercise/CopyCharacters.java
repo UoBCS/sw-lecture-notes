47c9480f56e075e5d3cf6d1dc9941f82ffd8b826
oss6 <ossedb@gmail.com>
2015-03-15 20:46:15 +0000

    Added exercise

---
+++ lec-io/src/exercise/CopyCharacters.java
@@ -0,0 +1,29 @@
+package exercise;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyCharacters {
+
+	public static void main(String[] args) throws IOException {
+		FileReader inputStream = null;
+		FileWriter outputStream = null;
+		
+		try {
+			inputStream = new FileReader("test.txt");
+			outputStream = new FileWriter("out_test.txt");
+			
+			int c;
+			while ((c = inputStream.read()) != -1)
+				outputStream.write(c);
+		}
+		finally {
+			if (inputStream != null)
+				inputStream.close();
+			if (outputStream != null)
+				outputStream.close();
+		}
+	}
+
+}
